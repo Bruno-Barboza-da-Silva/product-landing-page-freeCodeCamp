ae3dab65576852cbd9501ce44e31e2e0cc333ad5
Bruno <brunobarbozasilva@gmail.com>
2022-12-31 03:24:35 -0300

    Delete my-app directory

--- my-app/src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-    return(
-        <footer>        <p>This is a site made by Bruno Barboza</p>
-        <a href="https://github.com/Bruno-Barboza-da-Silva" target="_blank" rel="noreferrer">Github link</a>
-        </footer>
-    )
-}
-export default Footer
