7d229069931406438f4bc4a4ebc73feeaa7b478a
Bruno Barboza <brunobarbozasilva@gmail.com>
2023-01-01 11:11:27 -0300

    server criation

---
+++ product-landing-page/front-end/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import react from 'react'
+
+
+
+// export default api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+
+
+
+export default api = axios.create({
+    baseURL: 'http://localhost:5000/api',
+})
