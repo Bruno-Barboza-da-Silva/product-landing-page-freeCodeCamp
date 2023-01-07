d46685e67f7b976306a2c1eb539027bcad097221
Bruno Barboza <brunobarbozasilva@gmail.com>
2023-01-07 01:32:55 -0300

    adding display order

--- product-landing-page/back-end/controllers/managerController.js
+++ product-landing-page/back-end/controllers/managerController.js
@@ -4,7 +4,12 @@ sequelize.sync()
 
 const managerController = {
     index: async(request, response) => {
-          const clients = await ClientsModel.findAll();
+          const clients = await ClientsModel.findAll({
+            order: [
+                ['updated_at', 'DESC'],
+            ],
+          }
+          );
           console.log(clients)
           response.json(clients)
     }
